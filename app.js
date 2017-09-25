eae65d44f3131245e71de0490924d1dd99efb228
esther <estheralvaro44@gmail.com>
2017-09-25 18:05:20 -0500

    videos

---
+++ app.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const API_KEY = "AIzaSyB6RQPxv-X6aojxx9IKh0Nc4twyqlMnitI";
+
+let app = {
+   result: {
+      videos: [],
+      selectedVideo: null,
+      searchTerm: "iPhone X"
+   },
+
+   init: function() {
+      //app.videoSearch("iPhone");
+      app.youtubeSearch("iPhone X");
+   },
+   //<iframe className="embed-responsive-item" src={url}> </iframe>
+   getVideoList: function(videos) {
+      return videos.map((video, index) => {
+         const imageUrl = video.snippet.thumbnails.default.url;
+         const url = `https://www.youtube.com/embed/${video.id.videoId}`;
+         return `<li> 
+                   
+                     <p> 
+                        <iframe class="embed-responsive-item" src=${url}> </iframe>
+                     </p>
+               </li>`;
+      });
+   },
+   youtubeSearch: function(searchTerm) {
+      console.log(searchTerm);
+
+      YTSearch({ key: API_KEY, term: searchTerm }, data => {
+         console.log("result", data);
+         app.result = {
+            videos: data,
+            selectedVideo: data[0],
+            searchTerm: searchTerm
+         };
+         var list = app.getVideoList(app.result.videos);
+         console.log("lis: ", list);
+         $("#root").append(list);
+      });
+   },
+   videoSearch: function(searchTerm) {
+      jQuery.getJSON("list.json", data => {
+         console.log("result", data.items);
+         app.result = {
+            videos: data.items,
+            selectedVideo: data.items[0],
+            searchTerm: searchTerm
+         };
+         var list = app.getVideoList(app.result.videos);
+         console.log("lis: ", list);
+         $("#root").append(list);
+      });
+   }
+};
+
+$(document).ready(app.init);
